e09811c960a6ddc5395c3fe62cb9bb26c5861110
Zintaho <zintaho@gmail.com>
2022-01-13 11:07:27 +0900

    100 numpy exercise [11~20]

--- 100exercises.py
+++ 100exercises.py
@@ -37,3 +37,53 @@ print(Z)
 # 10. Find indices of non-zero elements from [1,2,0,0,4,0]
 nz = np.nonzero([1,2,0,0,4,0])
 print(nz)
+
+# 11. Create a 3x3 identity matrix
+Z = np.eye(3)
+print(Z)
+
+# 12. Create a 3x3x3 array with random values
+Z = np.random.random((3,3,3))
+print(Z)
+
+# 13. Create a 10x10 array with random values and find the minimum and maximum values
+Z = np.random.random((10, 10))
+Zmin, Zmax = Z.min(), Z.max()
+print(Zmin, Zmax)
+
+# 14. Create a random vector of size 30 and find the mean value
+Z = np.random.random(30)
+m = Z.mean()
+print(m)
+
+# 15. Create a 2d array with 1 on the border and 0 inside
+Z = np.ones((10, 10))
+Z[1:-1, 1:-1] = 0
+print(Z)
+
+# 16. How to add a border (filled with 0's) an existing array?
+Z = np.ones((5, 5))
+Z[:, [0, -1]] = 0
+Z[[0, -1], :] = 0
+print(Z)
+
+# 17. What is the result of the following expression?
+0 * np.nan; '''nan'''
+np.nan == np.nan; '''False'''
+np.inf > np.nan; '''False'''
+np.nan - np.nan; '''nan'''
+np.nan in set ([np.nan]); '''True'''
+0.3 == 3 * 0.1; '''False'''
+
+# 18. Create a 5x5 matrix with values 1,2,3,4 just below the diagonal
+Z = np.diag(1+ np.arange(4), k=-1)
+print(Z)
+
+# 19. Create a 8x8 matrix and fill it with a checkerboard pattern
+Z = np.zeros((8,8), dtype=int)
+Z[1::2, ::2] = 1
+Z[::2, 1::2] = 1
+print(Z)
+
+# 20. Consider a (6,7,8) shape array, what is the index (x,y,z) of the 100th element
+print(np.unravel_index(99, (6,7,8)))
